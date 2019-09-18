69de0f1fc909a18047e2d78997e040e233a3301e
Rahul Patwari <rpatwari@ingenuitygaming.com>
2019-09-18 11:24:51 +0530

    Run multiple instances of threejs apps.
    Disable antialiasing for better FPS.

--- main.js
+++ main.js
@@ -2,13 +2,16 @@ const pixelRatio = window.devicePixelRatio // 2 in case of retinas
 
 // Grab our canvases
 const canvas = document.querySelector('#app')
-const offscreenCanvas = document.createElement('canvas')
+const offscreenCanvas = document.createElement('canvas');
+const offscreenCanvas2 = document.createElement('canvas');
+
 
 // init pixi.js
 const pixiApp = initPixiApp(canvas)
 
 // init three.js offscreen
-const threeApp = initThreeApp(offscreenCanvas)
+const threeApp = initThreeApp(offscreenCanvas);
+const threeApp2 = initThreeApp2(offscreenCanvas2);
 
 /** background - PIXI */
 const texture = PIXI.Texture.from('https://picsum.photos/200/300');
@@ -34,6 +37,17 @@ pixiApp.stage.addChild(threeSprite)
 
 /** ======================================================= */
 
+// render threeApp2 as a pixi layer.
+const threeTexture2 = PIXI.Texture.from(threeApp2.renderer.domElement);
+const threeSprite2 = new PIXI.Sprite(threeTexture2);
+// divide by pixelratio to make it the correct size
+threeSprite2.scale.x /= pixelRatio;
+threeSprite2.scale.y /= pixelRatio;
+pixiApp.stage.addChild(threeSprite2);
+
+
+/** ======================================================= */
+
 /** foreground PIXI */
 const basicText2 = new PIXI.Text('Basic text in pixi');
 basicText2.x = 500;
@@ -50,7 +64,8 @@ function update(time) {
     const t = time * 0.001
 
     // THREE JS
-    threeApp.renderer.state.reset()
+    threeApp.renderer.state.reset();
+    // threeApp2.renderer.state.reset();
 
     // make the cubes rotate
     threeApp.scene.traverse(child => {
@@ -60,96 +75,31 @@ function update(time) {
         }
     })
 
-    threeApp.draw()
+    threeApp.draw();
+    // threeApp2.draw();
 
     threeApp.renderer.state.reset()
+    // threeApp2.renderer.state.reset()
 
 
     // PIXI
-    pixiApp.renderer.reset()
+    pixiApp.renderer.reset();
 
     // tell pixi that threejs has changed
-    threeSprite.texture.update()
+    threeSprite.texture.update();
+    threeSprite2.texture.update();
 
     pixiApp.render()
 
     pixiApp.renderer.reset()
     requestAnimationFrame(update)
 }
+
+/** begin the update loop. */
 requestAnimationFrame(update)
 
 
-
-
-
-
-
-
-function initThreeApp(canvas) {
-    const renderer = new THREE.WebGLRenderer({
-        canvas,
-        alpha: true,
-        antialias: true,
-    })
-
-    const fov = 45
-    const near = 0.01
-    const far = 100
-    const camera = new THREE.PerspectiveCamera(fov, 1, near, far)
-
-    const scene = new THREE.Scene()
-
-    function resize() {
-        const width = window.innerWidth
-        const height = window.innerHeight
-
-        renderer.setSize(width, height)
-        renderer.setPixelRatio(pixelRatio)
-
-        if (camera.isPerspectiveCamera) {
-            camera.aspect = width / height
-        }
-        camera.updateProjectionMatrix()
-    }
-
-    function draw() {
-        renderer.render(scene, camera)
-    }
-
-    // initial resize and draw
-    resize()
-    draw()
-
-    // add resize listener
-    window.addEventListener('resize', resize)
-
-    // add a light
-    const color = 0xFFFFFF
-    const intensity = 1
-    const light = new THREE.DirectionalLight(color, intensity)
-    light.position.set(-1, 2, 4)
-    scene.add(light)
-
-
-    // add some cubes
-    const boxWidth = 1
-    const boxHeight = 1
-    const boxDepth = 1
-    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
-    const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 })
-    const cube = new THREE.Mesh(geometry, material)
-    scene.add(cube)
-
-    camera.position.set(0, 0, 3)
-
-    return {
-        renderer,
-        camera,
-        scene,
-        resize,
-        draw,
-    }
-}
+/** ====================================================================================== */
 
 function initPixiApp(canvas) {
     return new PIXI.Application({
---
+++ threeApp.js
@@ -0,0 +1,65 @@
+function initThreeApp(canvas) {
+    const renderer = new THREE.WebGLRenderer({
+        canvas,
+        alpha: true,
+        antialias: false,
+    })
+
+    const fov = 45
+    const near = 0.01
+    const far = 100
+    const camera = new THREE.PerspectiveCamera(fov, 1, near, far)
+
+    const scene = new THREE.Scene()
+
+    function resize() {
+        const width = window.innerWidth
+        const height = window.innerHeight
+
+        renderer.setSize(width, height)
+        renderer.setPixelRatio(pixelRatio)
+
+        if (camera.isPerspectiveCamera) {
+            camera.aspect = width / height
+        }
+        camera.updateProjectionMatrix()
+    }
+
+    function draw() {
+        renderer.render(scene, camera)
+    }
+
+    // initial resize and draw
+    resize()
+    draw()
+
+    // add resize listener
+    window.addEventListener('resize', resize)
+
+    // add a light
+    const color = 0xFFFFFF
+    const intensity = 1
+    const light = new THREE.DirectionalLight(color, intensity)
+    light.position.set(-1, 2, 4)
+    scene.add(light)
+
+
+    // add some cubes
+    const boxWidth = 1;
+    const boxHeight = 1;
+    const boxDepth = 1;
+    const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+    const material = new THREE.MeshPhongMaterial({ color: 0x44aa88 });
+    const cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
+
+    camera.position.set(0, 0, 3);
+
+    return {
+        renderer,
+        camera,
+        scene,
+        resize,
+        draw,
+    }
+}
